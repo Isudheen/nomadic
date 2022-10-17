85a961bdf6496b9eb0ae19f490d2e7f6da0522cc
Isudheen <61105586+Isudheen@users.noreply.github.com>
2022-10-17 18:33:42 +0530

    class APIfeatures

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 const Tour = require('./../models/tourModel');
 
+//middleware for getting top tours
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
   req.query.sort = 'price,-ratingsAverage';
@@ -7,11 +8,14 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 };
 
-exports.getAllTours = async (req, res) => {
-  try {
-    //Build query
-    // 1A. Filtering
-    const queryObj = { ...req.query };
+class APIfeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
     const excludedFields = ['fields', 'sort', 'page', 'limit'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
@@ -23,40 +27,99 @@ exports.getAllTours = async (req, res) => {
     // { difficulty: 'easy', duration: { $gte: 5 } } because incoming query(from url - duration[gte]=5) ,will be like:
     // { difficulty: 'easy', duration: { gte: 5 } }
     // in regex \b for selecting exact words and g flag for all occurrence
-    let query = Tour.find(JSON.parse(queryStr));
-
-    //2. Sorting
-    if (req.query.sort) {
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
+  sort() {
+    if (this.queryString.sort) {
       const multiSort = req.query.sort.split(',').join(' '); // If current sort query is sort=price,-ratingsAverage
       //Splits the comma separated string into an array and join into one string with space b/w words. minus sign sorts in descending order.
-      query = query.sort(multiSort);
+      this.query = this.query.sort(multiSort);
       console.log(multiSort);
     } else {
-      query = query.sort('-createdAt'); //default sort. descending order of time
+      this.query = this.query.sort('-createdAt'); //default sort. descending order of time
     }
+    return this;
+  }
 
-    //3. Field limiting
-    if (req.query.fields) {
+  limitFields() {
+    if (this.queryString.fields) {
       const allFields = req.query.fields.split(',').join(' '); //---Inclusion projection If current query is fields=name,price ----name and price are included
-      query = query.select(allFields);
+      this.query = this.query.select(allFields);
       console.log(allFields);
     } else {
-      query = query.select('-__v'); //default condition. minus sign means exclusion projection __v is excluded. Cannot use inclusion and exclusion projections together.
+      this.query = this.query.select('-__v'); //default condition. minus sign means exclusion projection __v is excluded. Cannot use inclusion and exclusion projections together.
     }
+    return this;
+  }
+
+  paginate() {
+    // eg. page=2&limit=10...Here on page 1: 1st-10th entries, on page 2: 11th-20th entries etc...
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 10;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+}
+
+exports.getAllTours = async (req, res) => {
+  try {
+    //Build query
+    // 1A. Filtering
+    // const queryObj = { ...req.query };
+    // const excludedFields = ['fields', 'sort', 'page', 'limit'];
+    // excludedFields.forEach((el) => delete queryObj[el]);
+
+    // //1B. Advanced filtering
+    // let queryStr = JSON.stringify(queryObj);
+    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    // //replace matched expression with $ prepended to the expression, to get eg. $gte in MQL for mongo. eg.
+    // // { difficulty: 'easy', duration: { $gte: 5 } } because incoming query(from url - duration[gte]=5) ,will be like:
+    // // { difficulty: 'easy', duration: { gte: 5 } }
+    // // in regex \b for selecting exact words and g flag for all occurrence
+    // let query = Tour.find(JSON.parse(queryStr));
+
+    //2. Sorting
+    // if (req.query.sort) {
+    //   const multiSort = req.query.sort.split(',').join(' '); // If current sort query is sort=price,-ratingsAverage
+    //   //Splits the comma separated string into an array and join into one string with space b/w words. minus sign sorts in descending order.
+    //   query = query.sort(multiSort);
+    //   console.log(multiSort);
+    // } else {
+    //   query = query.sort('-createdAt'); //default sort. descending order of time
+    // }
+
+    //3. Field limiting
+    // if (req.query.fields) {
+    //   const allFields = req.query.fields.split(',').join(' '); //---Inclusion projection If current query is fields=name,price ----name and price are included
+    //   query = query.select(allFields);
+    //   console.log(allFields);
+    // } else {
+    //   query = query.select('-__v'); //default condition. minus sign means exclusion projection __v is excluded. Cannot use inclusion and exclusion projections together.
+    // }
 
     //4. Pagination
     // eg. page=2&limit=10...Here on page 1: 1st-10th entries, on page 2: 11th-20th entries etc...
-    if (req.query.page) {
-      const page = req.query.page * 1 || 1;
-      const limit = req.query.limit * 1 || 1;
-      const skip = (page - 1) * limit;
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-      query = query.skip(skip).limit(limit);
-    }
+    // if (req.query.page) {
+    //   const page = req.query.page * 1 || 1;
+    //   const limit = req.query.limit * 1 || 1;
+    //   const skip = (page - 1) * limit;
+    //   const numTours = await Tour.countDocuments();
+    //   if (skip >= numTours) throw new Error('This page does not exist');
+    //   query = query.skip(skip).limit(limit);
+    // }
 
     // Execute query
-    const tours = await query;
+    const features = new APIfeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    //chaining methods works because of "return this", which returns the entire object.
+    const tours = await features.query;
 
     //Sending response
     res.status(200).json({
