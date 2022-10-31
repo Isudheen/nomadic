a10e226e03c17386c0b7fea6b43b670bc775fd05
Isudheen <61105586+Isudheen@users.noreply.github.com>
2022-10-31 23:23:12 +0530

    Average ratings

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -48,5 +49,47 @@ reviewSchema.pre(/^find/, function (next) {
   next();
 });
 
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+reviewSchema.post('save', function () {
+  // this points to current review
+  //this.constructor.calcAvergeRatings is used instead of Review.calcAvergeRatings, because Review is not defined yet
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+//findOneAndUpdate or findOneAndDelete
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.findOne();
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // await this.findOne() does not work here. because query is already finished
+  await this.r.constructor.calcAverageRatings(this.r.tour);
+});
 const Review = mongoose.model('Review', reviewSchema);
 module.exports = Review;
