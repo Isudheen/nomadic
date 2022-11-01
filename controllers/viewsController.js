78062ea1c026ebf47b304cff842e098bd8368e5a
Isudheen <61105586+Isudheen@users.noreply.github.com>
2022-11-01 20:26:58 +0530

    tour-page building upto images

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -4,22 +4,32 @@ const catchAsync = require('../utils/catchAsync');
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) get Tour data from collection
   const tours = await Tour.find();
-
   // 2 ) Build template
 
   // 3) Render that template using tour data from step 1.
 
   res.status(200).render('overview', {
     title: 'All Tours',
+
     tours,
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) Get data for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  // 2) Build template
+
+  // 3) Render template using data from 1
+
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour,
   });
-};
+});
 
 exports.getLoginForm = catchAsync(async (req, res, next) => {
   res.status(200).render('login', {
