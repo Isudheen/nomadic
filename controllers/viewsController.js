1553d2b73b58d5c6f36f4238baa05f76465f2ab0
Isudheen <61105586+Isudheen@users.noreply.github.com>
2022-11-16 11:38:47 +0530

    Bookings done

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,4 +1,6 @@
+// const User = require('../models/userModel');
 const Tour = require('../models/tourModel');
+const Bookings = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
@@ -43,5 +45,19 @@ exports.getLoginForm = catchAsync(async (req, res, next) => {
 exports.getAccount = catchAsync(async (req, res, next) => {
   res.status(200).render('account', {
     title: 'Your account',
+  });
+});
+
+exports.getMyTours = catchAsync(async (req, res, next) => {
+  // 1) Find all bookings
+  const bookings = await Bookings.find({ user: req.user.id });
+
+  //2) Find tours with the returned IDs
+  const tourIDs = bookings.map((el) => el.tour);
+  const tours = await Tour.find({ _id: { $in: tourIDs } });
+
+  res.status(200).render('overview', {
+    title: 'My Tours',
+    tours,
   });
 });
