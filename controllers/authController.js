55130680cd51a6eedf0b2fe50911bafeea9bc9fe
Isudheen <61105586+Isudheen@users.noreply.github.com>
2022-11-24 09:43:16 +0530

    createSendTOken reverted

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,16 +12,16 @@ const signToken = (id) =>
   });
 //payload, secret and expiry respectively
 
-const createSendToken = (user, statusCode, req, res) => {
+const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
-  // if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
-  res.cookie('jwt', token, {
+  const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: req.secure || req.headers('x-forwarded-proto') === 'https',
-  });
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
 
   // Remove password from output
   user.password = undefined;
@@ -48,7 +48,7 @@ exports.signUp = catchAsync(async (req, res, next) => {
   const url = `${req.protocol}://${req.get('host')}/me`;
   await new Email(newUser, url).sendWelcome();
 
-  createSendToken(newUser, 201, req, res);
+  createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -69,7 +69,7 @@ exports.login = catchAsync(async (req, res, next) => {
     //401- unauthorized
   }
   // 3) If everything ok, send token to client
-  createSendToken(user, 200, req, res);
+  createSendToken(user, 200, res);
 });
 
 exports.logout = (req, res) => {
@@ -215,7 +215,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
   // 3) Update changedPasswordAt property for the user
   // 4) Log the user in, send JWT
-  createSendToken(user, 200, req, res);
+  createSendToken(user, 200, res);
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
@@ -232,5 +232,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   await user.save();
   //User.findByIdAndUpdate won't work as intended! pre save middlewares won't work....Also this reference in schema won't work
   // 4) Log users in , send JWT
-  createSendToken(user, 200, req, res);
+  createSendToken(user, 200, res);
 });
